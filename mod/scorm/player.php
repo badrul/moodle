59896c44406b3ce873d7b3b89a893ea5fbe80c1c
Badrul Nazri Mohamad <badrulnazri@hotmail.com>
2011-04-21 17:52:20 +0800

    Fixed javascript error if popup got blocked

--- mod/scorm/player.php
+++ mod/scorm/player.php
@@ -298,19 +298,24 @@
                         function openpopup(url,name,options,width,height) {
                             fullurl = "<?php echo $CFG->wwwroot.'/mod/scorm/' ?>" + url;
                             windowobj = window.open(fullurl,name,options);
-                            if ((width==100) && (height==100)) {
-                                // Fullscreen
-                                windowobj.moveTo(0,0);
-                            } 
-                            if (width<=100) {
-                                width = Math.round(screen.availWidth * width / 100);
-                            }
-                            if (height<=100) {
-                                height = Math.round(screen.availHeight * height / 100);
-                            }
-                            windowobj.resizeTo(width,height);
-                            windowobj.focus();
-                            return windowobj;
+							if (windowobj) {
+								if ((width==100) && (height==100)) {
+									// Fullscreen
+									windowobj.moveTo(0,0);
+								} 
+								if (width<=100) {
+									width = Math.round(screen.availWidth * width / 100);
+								}
+								if (height<=100) {
+									height = Math.round(screen.availHeight * height / 100);
+								}
+								windowobj.resizeTo(width,height);
+								windowobj.focus();
+								return windowobj;
+							} else {
+								 // The popup got blocked
+								return false;
+							}
                         }
 
                         url = "loadSCO.php?id=<?php echo $cm->id.$scoidpop ?>";
